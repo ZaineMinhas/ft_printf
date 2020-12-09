be35435a6c59bbe58c878fc60044ddf50a60f646
Zaine-el-abidine Minhas <zminhas@mi-r2-p1.s19.be>
2020-12-09 16:08:59 +0100

    mise a jour des exos

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,35 +6,46 @@
 /*   By: zminhas <zminhas@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/01 15:44:02 by zminhas           #+#    #+#             */
-/*   Updated: 2020/12/07 14:25:27 by zminhas          ###   ########.fr       */
+/*   Updated: 2020/12/09 15:32:09 by zminhas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_printf.h"
 
-static	int	ft_percent_all(const char *format, va_list args)
+static	int	ft_percent_all(const char **format, va_list args)
 {
-	format++;
-	if (*format == 'd' || *format == 'i')
+	if (**format == 'd' || **format == 'i')
 		return (ft_percent_d_and_i(va_arg(args, int)));
-	if (*format == 'u')
+	if (**format == 'u')
 		return (ft_percent_u(va_arg(args, unsigned int)));
-	if (*format == 'x')
+	if (**format == 'x')
 		return (ft_percent_x(va_arg(args, unsigned int)));
-	if (*format == 'X')
+	if (**format == 'X')
 		return (ft_percent_xcap(va_arg(args, unsigned int)));
-	if (*format == 'c')
+	if (**format == 'c')
 		return (ft_percent_c(va_arg(args, int)));
-	if (*format == 's')
+	if (**format == 's')
 		return (ft_percent_s(va_arg(args, char *)));
-	if (*format == 'p')
+	if (**format == 'p')
 		return (ft_percent_p(va_arg(args, void *)));
-	if (*format == '%')
+	if (**format == '%')
 	{
 		write(1, "%", 1);
 		return (1);
 	}
 	return (0);
+}
+
+static	int	ft_flagchecker(const char **format, va_list args)
+{
+	*format++;
+	if (**format == '-')
+		return (ft_flag_minus(*format, va_arg(args, int)));
+	if (**format == '0' || **format == '.')
+		return (ft_flag_zero_and_point(*format, va_arg(args, int)));
+	if (**format == '*')
+		return (ft_flag_star(*format, va_arg(args, int)));
+	return (ft_percent_all(format, args));
 }
 
 int			ft_printf(const char *format, ...)
@@ -48,7 +59,7 @@ int			ft_printf(const char *format, ...)
 	{
 		if (*format == '%')
 		{
-			i += ft_percent_all(format, args);
+			i += ft_flagchecker(&format, args);
 			format++;
 		}
 		else
